ee0d8147b7b0f0316c747b5788c15f45c111edee
mochi <whoatemyapplepie@gmail.com>
2023-07-17 16:20:25 +0900

    make sample connect client using golang.

---
+++ connect-go-example/cmd/client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+
+	"github.com/bufbuild/connect-go"
+)
+
+func main() {
+	client := greetv1connect.NewGreetServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+	)
+	res, err := client.Greet(
+		context.Background(),
+		connect.NewRequest(&greetv1.GreetRequest{
+			Name: "Shibushi",
+		}),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res.Msg.Greeting)
+}
