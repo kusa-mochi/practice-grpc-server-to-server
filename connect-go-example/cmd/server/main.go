a8f5f2a6008a6ff4de6a20f481fd41ee4c62c3da
mochi <whoatemyapplepie@gmail.com>
2023-07-17 15:54:51 +0900

    make sample connect server using golang.

---
+++ connect-go-example/cmd/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+
+	"golang.org/x/net/http2/h2c"
+
+	"golang.org/x/net/http2"
+)
+
+type GreetServer struct{}
+
+func (s *GreetServer) Greet(
+	ctx context.Context,
+	req *connect.Request[greetv1.GreetRequest],
+) (*connect.Response[greetv1.GreetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&greetv1.GreetResponse{
+		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
+	mux.Handle(path, handler)
+	http.ListenAndServe(
+		"localhost:8080",
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
